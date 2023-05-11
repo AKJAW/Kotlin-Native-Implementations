0a1f6b08f1476ed5cb51c625c7011fd8b2abfe61
AKJAW <akjaworski1@gmail.com>
2023-05-11 17:46:35 +0200

    feat: enable compose

--- shared/src/iosMain/kotlin/main.ios.kt
+++ shared/src/iosMain/kotlin/main.ios.kt
@@ -1,5 +1,5 @@
-//import androidx.compose.ui.window.ComposeUIViewController
+import androidx.compose.ui.window.ComposeUIViewController
 
 actual fun getPlatformName(): String = "iOS"
 
-//fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { App() }
